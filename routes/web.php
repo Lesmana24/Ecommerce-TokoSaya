7966747b3e77a7cc04e8b538ad8c12772d7d3846
Lesmana <lesmana24@student.polindra.ac.id>
2025-12-13 23:59:51 +0700

    Midleware Admin

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,11 @@ Route::get('/products/{product}', [ProductController::class, 'show'])->name('pro
 | ROUTE ADMIN (Harus Login)
 |--------------------------------------------------------------------------
 */
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
     // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
